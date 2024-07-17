e8d66c7a3ddbd4ed58c9d1cdb21887bd699f6eac
Praza <keelahuagain@gmail.com>
2024-07-17 18:42:25 +0300

    Configured gitignore, added HealthComponent

---
+++ quantum_code/quantum.code/Platformer/Systems/FallDamageSystem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quantum.Platformer
+{
+    public unsafe class FallDamageSystem : SystemMainThreadFilter<FallDamageSystem.Filter>
+    {
+        public struct Filter
+        {
+            public EntityRef Entity;
+            public Transform3D* Transform3D;
+            public CharacterController3D* CharacterController;
+            public PlayerPlatformController* PlatformController;
+            public HealthComponent* HealthComponent;
+        }
+
+        public override void Update(Frame f, ref Filter filter)
+        {
+            Log.Info(filter.HealthComponent->currentHealth);
+        }
+
+       }
+}
